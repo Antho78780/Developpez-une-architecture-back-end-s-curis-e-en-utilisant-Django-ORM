f3565005f40f48ed8aa7cb2e3a8fc189c9e3c7cb
Anthony Audrain <anthonyaud78@gmail.com>
2023-07-04 02:48:07 +0200

    mise à jour des attributs

--- epicEvent/contract/models.py
+++ epicEvent/contract/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from authentication.models import User
 
 class Customer(models.Model):
     firstname = models.CharField(max_length=25)
@@ -6,19 +7,25 @@ class Customer(models.Model):
     email = models.CharField(max_length=100)
     mobile = models.CharField(max_length=20)
     compagny_name = models.CharField(max_length=100)
-    date_created = models.DateField()
-    date_updated = models.DateField()
+    sales_contact = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
 
 
 class Contract(models.Model):
-    contract_status = models.BooleanField()
-    amount = models.IntegerField()
+    amount = models.FloatField()
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    date_created = models.DateField()
-    date_updated = models.DateField()
+    status = models.BooleanField()
+    payment_due = models.DateTimeField()
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
 
 
 class Event(models.Model):
-    contract = models.OneToOneField(Contract, on_delete=models.CASCADE)
-    date_created = models.DateField()
-    date_updated = models.DateField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    attendees = models.IntegerField()
+    support_contact = models.ForeignKey(User, on_delete=models.CASCADE)
+    notes = models.CharField(max_length=300)
+    event_date = models.DateTimeField()
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
