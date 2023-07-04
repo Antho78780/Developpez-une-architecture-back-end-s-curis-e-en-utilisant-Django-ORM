f8f3ea559dc648543e604434a16e109cd1ccdd46
Anthony Audrain <anthonyaud78@gmail.com>
2023-07-04 02:43:33 +0200

    ajout du choix d'autorisation

--- epicEvent/authentication/models.py
+++ epicEvent/authentication/models.py
@@ -1,6 +1,11 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
-    pass
+   autorisations_choice = [
+      ("MA", "managment_team"),
+      ("SA", "sales_team"),
+      ("SU", "support_team")
+   ]
+   autorisations = models.CharField(choices=autorisations_choice, default="MA")
 
