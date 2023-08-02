07be1f04bb987ae9a48e071b43ec6d33dbcde1d2
Anthony Audrain <anthonyaud78@gmail.com>
2023-08-02 18:46:12 +0200

    petite modif au niveau du port

--- epicEvent/epicEvent/settings.py
+++ epicEvent/epicEvent/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': '5341',
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 LOGGING = {
